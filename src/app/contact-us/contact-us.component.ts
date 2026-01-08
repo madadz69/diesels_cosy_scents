188044a5afcfcbba768445980d7415cb7ac2bcb9
Adam Jenkins <adam.jenkins69@me.com>
2026-01-08 12:12:57 +0000

    About us and contact us

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+  FormData!: FormGroup;
 
+  constructor(private builder: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  onSubmit(): void {
+    const { fullname, email, message } = this.FormData.value;
+  }
+
+  private buildForm(): void {
+    this.FormData = this.builder.group({
+      fullname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required])
+    })
+  }
 }
