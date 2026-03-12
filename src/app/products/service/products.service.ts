d3678e50a550976272bf6b3d66f393cd637d9675
Adam Jenkins <adam.jenkins69@me.com>
2026-03-12 11:24:17 +0000

    Add parameters

--- src/app/products/service/products.service.ts
+++ src/app/products/service/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse, HttpStatusCode } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 
@@ -12,14 +12,32 @@ export class ProductsService {
   // Create the service URL placeholder
   serviceUrl = "http://localhost:8080";
   
-  // hit the endpoint and get the products
+
+  /**
+   * Get all products
+   * @returns {Observable<Product[]>} Observable of product array
+   */
   getData(): Observable<Product[]> {
     const data = this.http.get<Product[]>(this.serviceUrl + "/products");
     return data;
   }
 
+  /**
+   * Search for products
+   * @param {string} searchTerm - Term user is searching
+   * @returns {Observable<Product[], HttpStatusCode>} Observable of product array and status
+   */
   searchProduct(searchTerm: string): Observable<Product[]> {
     const params = new HttpParams().set('q', searchTerm);
     return this.http.get<Product[]>(`${this.serviceUrl}/products/search`, { params });
+  }
+
+  /**
+   * Delete the product - ADMIN ONLY
+   * @param id - the product ID to delete
+   * @returns {Observable<void>} void response.
+   */
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.serviceUrl}/products/${id}`);
   }
 }
